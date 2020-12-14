523747449296a75567299c4cd81a75beb370f32d
Jim Doran <jimdoran64@gmail.com>
2020-12-14 15:53:59 -0500

    Update jim1.py

--- jim1.py
+++ jim1.py
@@ -1,58 +1,101 @@
+#<span data-placement="top" title=
+
+#  this pulls names of people's from the scsa world challenge squad list even if they say reserved when viewed in practiscore   .
+
 import requests
 from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+from html.entities import name2codepoint
 
+count =0
 import time
 import sys
 from os import system
-print ("aggg")
+print ("jd starting jim1.py")
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-#print ("Start : %s"  time.ctime())
+class MyHTMLParser(HTMLParser):
+    
+    def handle_starttag(self, tag, attrs):
+#        print("Start tag:", tag)
+        for i in range(len(attrs)  )  :
+#            if (i==2 or i==3 or i==7):
+             if ( i==7):
+                print( i  , attrs[i])
+            
+               
+print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+               
 
-url = 'https://practiscore.com/world-speed-shooting-championship-2020/squadding'
+#system('say emily  do you want dessert  ' + str(10)  )
+print ("Start : %s"  % time.ctime())
+url ='https://practiscore.com/world-speed-shooting-championship-2021-presented-by-psa/squadding'
 print ( url )
-
-#res = requests.get(url)
-
 
 html_text = requests.get(url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
-print ("jjj")
+
+"""
+<span data-placement="top" disabled="disabled" id="squad_452038_1_117990" name="squad_452038_1_117990" placeholder="reserved" rel="1" style="background-color: #fcf8e3;" title="Zack Jones, JR (PCC Optic)">
+<small class="text-muted">Reserved</small>
+</span>
+
+"""
+
+#print(soup.select_one("span[title*=Kurt]"))
+
+#jjj =str ( soup.select_one('span[data-placement="top"]')  )
+
+parser = MyHTMLParser()
+
+#for kkk in soup.find_all('span', {'data-placement':'top'}):
+for iteration, kkk in  enumerate ( soup.find_all('span', {'data-placement':'top'})  ) :
+
+    #print kkk  # raw html with span tags
+    print ( parser.feed( str(kkk)  ) )
+    #print ( iteration+1)
+                
+print ("Total")
+print ( str(iteration+1))
+
+
+
+quit()
 
 
 
 
 
-#html = open("medium.html").read()
-#soup = BeautifulSoup(html_text)
+"""
 
-mydivs = soup.find('div', {'class' :'squadBox'}).text
-print ( mydivs )
-
-#for div in mydivs:
-    #print div
- #   print ("dddd")
+<span data-placement="top" disabled="disabled" id="squad_452038_1_117990" name="squad_452038_1_117990" placeholder="reserved" rel="1" style="background-color: #fcf8e3;" title="Zack Jones, JR (PCC Optic)">
+<small class="text-muted">Reserved</small>
+</span>
+"""
 
 
 
-
-
-tag = soup.find( text="Empty")
+"""
 
 
 print ("aaaaa")
 aa=soup.find('div', {'class' :'squadBox'}).text
 print (aa )
-
+"""
 
 
 #print tag.find_parent('div')
-
-
-
-
-
 
 
 
@@ -71,11 +114,6 @@ for tr in rows:
     print
 
 
-
-
-
-
-
 tables = [
     [
         [td.get_text(strip=True) for td in tr.find_all('td')]
@@ -90,14 +128,5 @@ tables = [
 
 #print(soup.title)
 print (soup.title.text)
-
-
-
-
-
-
-#text = soup.get_text()
-
-print ("end ")
 
 """
